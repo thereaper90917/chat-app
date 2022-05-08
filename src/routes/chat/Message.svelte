44db8dacd7b5e537f4c2c10bf927e0072bfadad5
Dragonium <drago@dragonium.nl>
2022-05-08 09:40:39 +0200

    made message component

---
+++ src/routes/chat/Message.svelte
@@ -0,0 +1,40 @@
+<script>
+  export let message;
+
+  const timestamp = new Date(Date.parse(message.data.timestamp));
+
+</script>
+
+<div class="chat-message-left pb-4">
+  <div>
+    <img
+      src="https://bootdey.com/img/Content/avatar/avatar3.png"
+      class="rounded-circle mr-1"
+      alt="Sharon Lessman"
+      width="40"
+      height="40"
+    />
+    <div class="text-muted small text-nowrap mt-2">
+      {timestamp.toLocaleTimeString()}
+    </div>
+  </div>
+  <div>
+    <div class="flex-grow-1 ml-3">
+      {message.user.name}
+    </div>
+    <div class="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
+      {#if message.gif.included}
+        <img src={message.gif.url} alt="" width="300px" height="200px" />
+      {:else if message.youtube.included}
+        <iframe
+          title="Youtube Video"
+          width="420"
+          height="345"
+          src={message.youtube.url}
+        />
+      {:else}
+        {message.text}
+      {/if}
+    </div>
+  </div>
+</div>
--- src/routes/home/Chat.svelte
+++ src/routes/home/Chat.svelte
@@ -1,27 +1,23 @@
 <script>
   import io from "socket.io-client";
-  import Gifs from './Gifs.svelte'
+  import Gifs from "./Gifs.svelte";
+  import Message from "../chat/Message.svelte";
   import { preferences } from "../../stores/users";
-  import EmojiPicker from 'svelte-emoji-picker';
+  import EmojiPicker from "svelte-emoji-picker";
 
   const socket = io("http://45.41.204.198:8070/");
-  let showEmoji = false
-  let showGifs = false
-  let current = new Date();
+  let showEmoji = false;
+  let showGifs = false;
   let messages = [];
   let message = "";
   let userName = $preferences.user.name;
-  
-  socket.on("chat message", (user, msg) => {
 
-    if(msg.text === ''){
-      return
+  socket.on("chat message", (user, msg) => {
+    if (msg.text === "") {
+      return;
     }
     messages = [...messages, msg];
-    // userName = user[0].name
 
-    // userName = user[0].name
-    // console.log(user)
     setTimeout(() => {
       const chatMessages = document.querySelector(".chat-messages");
       chatMessages.scrollTop = chatMessages.scrollHeight;
@@ -29,8 +25,10 @@
   });
 
   function sendMessage() {
-   
     const sendMessage = {
+      data: {
+        timestamp: new Date().toISOString(),
+      },
       text: message,
       user: {
         name: userName,
@@ -39,18 +37,21 @@
         inlcuded: false,
         url: "",
       },
-      youtube:{
+      youtube: {
         inlcuded: false,
         url: "",
-      }
+      },
     };
-    if(sendMessage.text.includes('https://media0.giphy.com')){
-      sendMessage.gif.included = true
-      sendMessage.gif.url = message
+    if (sendMessage.text.includes("https://media0.giphy.com")) {
+      sendMessage.gif.included = true;
+      sendMessage.gif.url = message;
     }
-    if(sendMessage.text.includes('https://www.youtube.com/')){
-      sendMessage.youtube.included = true
-      sendMessage.youtube.url = message.replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/")
+    if (sendMessage.text.includes("https://www.youtube.com/")) {
+      sendMessage.youtube.included = true;
+      sendMessage.youtube.url = message.replace(
+        "https://www.youtube.com/watch?v=",
+        "https://www.youtube.com/embed/"
+      );
     }
     socket.emit("chat message", sendMessage);
     message = "";
@@ -84,40 +85,7 @@
   <div class="position-relative">
     <div class="chat-messages p-4">
       {#each messages as message}
-        <div class="chat-message-left pb-4">
-          <div>
-            <img
-              src="https://bootdey.com/img/Content/avatar/avatar3.png"
-              class="rounded-circle mr-1"
-              alt="Sharon Lessman"
-              width="40"
-              height="40"
-            />
-            <div class="text-muted small text-nowrap mt-2">
-              {current.toLocaleTimeString()}
-            </div>
-          </div>
-          <div>
-            <div class="flex-grow-1 ml-3">
-              {message.user.name}
-            </div>
-            <div class="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
-              {#if message.gif.included}
-                <img
-                  src={message.gif.url}
-                  alt=""
-                  width="300px"
-                  height="200px"
-                />
-                {:else if message.youtube.included}
-                <iframe title="Youtube Video" width="420" height="345" src="{message.youtube.url}">
-                </iframe>
-              {:else}
-                {message.text}
-              {/if}
-            </div>
-          </div>
-        </div>
+        <Message {message} />
       {/each}
     </div>
   </div>
@@ -132,16 +100,25 @@
         bind:value={message}
       />
       <button class="btn btn-primary" on:click={sendMessage}>Send</button>
-      <button class="btn btn-primary" on:click={()=> (showGifs === true) ? showGifs = false : showGifs = true}>Gifs</button>
-      <button class="btn btn-primary" on:click={()=> (showEmoji === true) ? showEmoji = false : showEmoji = true}>Emoji</button>
+      <button
+        class="btn btn-primary"
+        on:click={() =>
+          showGifs === true ? (showGifs = false) : (showGifs = true)}
+        >Gifs</button
+      >
+      <button
+        class="btn btn-primary"
+        on:click={() =>
+          showEmoji === true ? (showEmoji = false) : (showEmoji = true)}
+        >Emoji</button
+      >
     </div>
-    {#if  showGifs }
-    <Gifs showGifs = {showGifs}/>
+    {#if showGifs}
+      <Gifs {showGifs} />
     {/if}
-    
+
     {#if showEmoji}
-    <EmojiPicker bind:value={message} />
+      <EmojiPicker bind:value={message} />
     {/if}
-    
   </div>
 </div>
